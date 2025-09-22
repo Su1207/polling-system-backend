1d28866c32a27908cde0d86bfaae4553048f3205
suraj maheshwari <surajmaheshwari159@gmail.com>
2025-09-23 01:37:11 +0530

    refetch data socket

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import express from "express";
 import { app, server } from "./lib/socket.js";
 import dotenv from "dotenv";
 import { connectDB } from "./db/db.js";
-import pollRouter from "./routes/pollRoute.js";
 
 dotenv.config();
 
@@ -11,7 +10,6 @@ const PORT = process.env.PORT || 3000;
 connectDB();
 
 app.use(express.json());
-app.use("/api", pollRouter);
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- routes/pollRoute.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { createPoll } from "../controllers/pollController.js";
-
-const router = express.Router();
-
-router.post("/create-poll", createPoll);
-
-export default router;
